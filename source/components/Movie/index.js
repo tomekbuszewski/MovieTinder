50357835c4bece18d89ebb0740bdc0ba9a27bb54
Tomek Buszewski <tomek.buszewski@gmail.com>
2018-01-03 18:14:16 +0100

    Movie component started

--- source/components/Movie/index.js
+++ source/components/Movie/index.js
@@ -6,6 +6,12 @@ import c from '../../services/classNameBuilder';
 
 import style from './style.scss';
 
+/**
+ * Class name builder for this particular component
+ * @param {string|undefined} e - element
+ * @param {string|undefined} m - modifier
+ * @returns {string} - class name from given style
+ */
 const cn = (e = undefined, m = undefined) => c(style, { b: 'Movie', e, m });
 
 const Movie = ({ cover, title, description, rating }: { id: Number, cover: String | Boolean, title: String, description: String, rating: any }) =>
