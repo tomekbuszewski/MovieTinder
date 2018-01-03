4501d509b8456e8d9bf3ac0fa7ae1b62b571f8ab
Tomek Buszewski <tomek.buszewski@gmail.com>
2018-01-03 19:41:32 +0100

    CNB as a dep

--- source/components/Movie/index.js
+++ source/components/Movie/index.js
@@ -1,21 +1,13 @@
 // @flow
 import React from 'react';
-
-/* Services */
-import c from '../../services/classNameBuilder';
+import Cn from 'bem-classname-builder';
 
 import style from './style.scss';
 
-/**
- * Class name builder for this particular component
- * @param {string|undefined} e - element
- * @param {string|undefined} m - modifier
- * @returns {string} - class name from given style
- */
-const cn = (e = undefined, m = undefined) => c(style, { b: 'Movie', e, m });
+const bem = new Cn(style, 'Movie');
 
 const Movie = ({ cover, title, description, rating }: { id: Number, cover: String | Boolean, title: String, description: String, rating: any }) =>
-  <article className={cn()}>
+  <article className={bem.get()}>
     <h2>{title}</h2>
     {cover && <img src={cover} alt={title} title={title} />}
     <p>{description}</p>
